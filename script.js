087c2bffcacef1fa1d76068fa132596e293be16c
Tim Dorsman <tim.dorsman@hotmail.com>
2019-04-09 16:43:21 +0200

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const url = 'api.openweathermap.org/data/2.5/forecast/hourly?id=524901';
-
+const url = 'http://api.apixu.com/v1/current.json?key=3a3ab57674994be3977143414190904&q=zoetermeer';
 fetch(url)
 .then(res => res.json())
 .then(data => {
 	console.log(data);
+	const { current, location } = data;
+
+	const result = document.querySelector('.result');
+
+	if(current.condition.text === 'Sunny') {
+		result.innerText = 'Ja';
+	}
+	else {
+		result.innerText = 'Nee';
+	}
 })
