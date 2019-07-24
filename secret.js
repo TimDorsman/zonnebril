9c7a5f54a3f2b502aed12dda20a48b22f9fb284c
Tim Dorsman <31539107+TimDorsman@users.noreply.github.com>
2019-07-24 09:24:06 +0200

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,6 +1,6 @@
 const letters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','!','?','[',']',':',';','/','-','~','@','$','^','%','*','#',',','.',' ',
 'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
-let p = 'luuk waar ben je nou mee bezig, ik word echt gek door jou';
+let p = '';
 
 let counter = 0;
 let codeString;
@@ -19,8 +19,8 @@ function createEncryption() {
 	for(i = 0; i < p.length; i++) {
 		let random = Math.floor(Math.random() * letters.length);
 		guessString.push(letters[random])
-		let cheese = guessString.toString().replace(/,/g, '')
-		document.querySelector('.key').innerText = cheese;
+		let newGuessString = guessString.toString().replace(/,/g, '')
+		document.querySelector('.key').innerText = newGuessString;
 	}
 }
 
@@ -32,8 +32,8 @@ function changeEncryption() {
 			let random = Math.floor(Math.random() * letters.length);
 			guessString.splice(i, 1, letters[random]);
 
-			let cheese = guessString.toString().replace(/,/g, '')
-			document.querySelector('.key').innerText = cheese;
+			let newGuessString = guessString.toString().replace(/,/g, '')
+			document.querySelector('.key').innerText = newGuessString;
 			correct = 0;
 		}
 		else {
